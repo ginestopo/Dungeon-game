e9be31b8e678b9eed0359790970aaf2242dc19c2
Ginés Díaz <ginestopo@gmail.com>
2022-01-22 21:33:40 +0100

    Added bounce effect on coins
    Added bounce effect on coins, using a simple sine function that changes with time using delta in bounce_effect() function

--- scenes/objects/coin/Coin.gd
+++ scenes/objects/coin/Coin.gd
@@ -5,23 +5,42 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var random = RandomNumberGenerator.new()
 
+
+var initial_position = Vector2(0.0,0.0)
+var initial_argument
+var argument
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Area2D.visible = false
+	initial_position = self.position
+	random.randomize()
+	initial_argument = random.randf_range(0.0, PI)
+	argument = initial_argument
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+func _process(delta):
+	bounce_effect(delta)
 
 func _on_Area2D_body_entered(body):
 	if(body.name == "Player"):
 		$AnimationPlayer.play("taken")
 	
+	
+func bounce_effect(delta):
+	
+	self.position.y = initial_position.y + 80*sin(argument)*delta
+	
+	if(argument>2*PI):
+		argument = initial_argument
+	else:
+		argument += 0.1
+		
+		
 func delete_node():
 	queue_free()
+
 	
 
