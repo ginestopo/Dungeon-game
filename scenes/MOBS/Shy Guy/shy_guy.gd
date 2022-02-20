b2dcee0950a8f630c9be15ebbe63e9ecec9542ce
Ginés Díaz <ginestopo@gmail.com>
2022-02-20 19:17:56 +0100

    Implementing new mob
    Implementing new mob

---
+++ scenes/MOBS/Shy Guy/shy_guy.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+class_name ShyGuy , "res://0x72_DungeonTilesetII_v1.4/frames/skelet_idle_anim_f0.png"
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
